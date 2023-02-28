293846338b1a0c52ee2603e60ca37ad9245d7aec
kjhchief <kjhchief@naver.com>
2023-02-28 17:34:53 +0900

    23/2/28_2

--- src/main/java/ezen/servlet/BoardListServlet.java
+++ src/main/java/ezen/servlet/BoardListServlet.java
@@ -1,6 +1,11 @@
 package ezen.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class BoardListServlet
+ * Database
  */
 @WebServlet("/board/list")
 public class BoardListServlet extends HttpServlet {
@@ -18,8 +23,48 @@ public class BoardListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 응답메시지에 Content-Type 헤더에 전송하고자 하는 컨텐츠 종류 설정
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		
+		// DB에서 조회된 데이터라고 가정
+		List<String> boardList = new ArrayList<>();
+		boardList.add("오늘 회식입니다.");
+		boardList.add("서블릿 어려워요 ㅠㅠ");
+		boardList.add("다음에 JSP 배웁니다.");
+		
+		// out을 통해 브라우저에게 출력
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>게시판 리스트</title>");
+		out.println("<link rel='stylesheet' href='/servlet/css/table.css'>");
+		out.println("</head>");
+		
+		out.println("<body>");
+		
+		out.println("<h1>게시판 목록</h1>");
+		
+		out.println("<table border='1'>");
+		out.println("<tr>");
+		out.println("<th>번호</th>");
+		out.println("<th>게시판 제목</th>");
+		out.println("</tr>");
+		
+		int number = 0;
+		for (String title : boardList) {
+			number++;
+			out.println("<tr>");
+			out.println("<td>"+number+"</td>");
+			out.println("<td>"+title+"</td>");
+			out.println("</tr>");	
+			
+		}
+		
+		
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
